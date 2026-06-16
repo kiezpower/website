313faebca5a56f0103429e10d4cadf10ebd06320
Christian Roessler <christian.roessler@cmdscale.com>
2026-06-16 08:53:01 +0200

    waitlist email, github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'static',
-  site: 'https://DEIN-USERNAME.github.io', // ← anpassen
-  base: '/',
+  output: "static",
+  site: "https://kiezpower.github.io",
+  base: "/",
 });
