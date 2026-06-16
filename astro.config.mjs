03fdc552412afe14f0dc59defc7174c1d59ff56e
Christian Roessler <christian.roessler@cmdscale.com>
2026-06-16 14:19:08 +0200

    supabase vars in env, astro sitemap module, plz astro page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: "static",
   site: "https://kiez-power.de",
   base: "/",
+  integrations: [sitemap()],
 });
